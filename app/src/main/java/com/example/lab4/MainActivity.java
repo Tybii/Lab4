e7198c151ad9ae134415e79a125ff8637b3e9744
Your NameTyberiusz <you@example.comfrymus.tyberiusz@gmail.comgit config --global user.name Your>
2020-05-23 21:35:43 +0200

    Lab5 Wyświetlanie wpisów

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -8,10 +8,11 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.SimpleCursorAdapter;
 import android.widget.ListView;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+
 
 
 import java.util.ArrayList;
@@ -20,12 +21,13 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> target;
-    private ArrayAdapter adapter;
+    private SimpleCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MySQLite db = new MySQLite(this);
 
         target = new ArrayList<>();
         String[] values = new String[]{
@@ -34,7 +36,10 @@ public class MainActivity extends AppCompatActivity {
         };
 
         target.addAll(Arrays.asList(values));
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+        adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2,
+                db.lista(), new String[]{"_id", "gatunek"},
+                new int[]{android.R.id.text1, android.R.id.text2},
+                SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE);
 
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(this.adapter);
